66a78752eac622c39ca04763cd6870b4cb6c50f1
Thanh Nam <nampt.dev@gmail.com>
2021-12-05 15:46:07 +0700

    Init source code

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data () {
+        return {
+            msg: 'Nam PT'
+        }
+    }
+}
+</script>
